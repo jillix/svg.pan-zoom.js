ecae851ccff03f584e6145591793dbb6a4e484e1
Ionică Bizău <bizauionica@yahoo.com>
2015-01-06 11:40:22 +0200

    Added the demo directory.

---
+++ demo/js/handlers.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var svg = SVG($(".graph").get(0)).size("100%", "100%");
+    var links = svg.group();
+    var markers = svg.group();
+    var nodes = svg.group();
+
+    var g1 = nodes.group().translate(300, 200).draggable();
+    g1.circle(80).fill("#C2185B");
+
+    var g2 = nodes.group().translate(100, 200).draggable();
+    g2.circle(50).fill("#E91E63");
+
+    var g3 = nodes.group().translate(200, 400).draggable();
+    g3.circle(100).fill("#FF5252");
+
+    nodes.panZoom();
+});
